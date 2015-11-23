afb65f74f65568f89c2904e4182d049a200b1e8e
timDaBone <tim@boehnel.io>
2015-11-23 21:50:14 +0100

    added connection timeout

--- start_distribution_server.bat
+++ start_distribution_server.bat
@@ -1,4 +1,4 @@
 cd "build\classes\"
 start cmd /K rmiregistry 1099
 timeout 3
-start cmd /K java -Djava.security.policy=file:/server.policy -Dsun.rmi.transport.tcp.responseTimeout=1000 vss.a4.server.DistributionServer
+start cmd /K java -Djava.security.policy=file:/server.policy -Dsun.rmi.transport.connectionTimeout=1000 -Dsun.rmi.transport.tcp.responseTimeout=1000 vss.a4.server.DistributionServer
